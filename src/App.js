d877474b8a52b98310d460ca3fdcf2778b0c1983
J3m1th <denys.petryniak@gmail.com>
2022-08-25 16:48:53 +0300

    Adding a new user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import UserTable from "./tables/UserTable";
+import AddUserForm from "./forms/AddUserForm";
 
 const App = () => {
   const usersData = [
     { id: 1, name: "Denys", username: "dpetryniak" },
     { id: 2, name: "Vlad", username: "alex" },
     { id: 3, name: "Lera", username: "kitten" },
+    { id: 4, name: "Dimon", username: "domestos" },
   ];
 
   const [users, setUsers] = useState(usersData);
+
+  const addUser = user => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
 
   return (
     <div className="container">
@@ -16,6 +23,7 @@ const App = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add User</h2>
+          <AddUserForm addUser={addUser} />
         </div>
         <div className="flex-large">
           <h2>View users</h2>
